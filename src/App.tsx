565b8be9f47dd6afee98568817a9d856b93e653a
Ellen DUAN <ellenduan@Ellens-MacBook-Pro.local>
2023-12-04 17:37:47 -0500

    Updated footer

--- src/App.tsx
+++ src/App.tsx
@@ -27,10 +27,10 @@ const App: FC<any> = () => {
                             </main>
                             <footer className='py-5'>
                                 <div className="d-flex justify-content-center align-items-center text-muted">
-                                    RHEACT is currently under development and we anticipate a stable Beta version by Summer 2023
+                                    RHEACT is currently under development and we anticipate a stable Beta version by Spring 2024
                                 </div>
                                 <div className="d-flex justify-content-center align-items-center text-muted">
-                                    Reactive Hazard Evaluation Analysis and Compilation Tool (RHEACT) v3.0 (Beta version), May 9, 2023
+                                    Reactive Hazard Evaluation Analysis and Compilation Tool (RHEACT) v3.0 (Beta version), Dec 1, 2023
                                     {/* &copy; CISTAR, {new Date().getFullYear()} */}
                                 </div>
                             </footer>
